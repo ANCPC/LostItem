3fec2f371c24188ed93f090590b0bc9db614fea9
ANCPC <creaturepc1@gmail.com>
2026-04-11 12:01:15 +0100

    Added main file

--- CODE/user_db.c
+++ CODE/user_db.c
@@ -8,9 +8,14 @@ struct User {
     int points;
 };
 
-// Add user to file (database)
+// Add user to file
 void addUser() {
-    FILE *fp = fopen("users.txt", "a");
+    FILE *fp = fopen("users.txt", "ab");  // binary append
+
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
 
     struct User u;
 
@@ -20,7 +25,7 @@ void addUser() {
     printf("Enter Name: ");
     scanf("%s", u.name);
 
-    u.points = 0;  // initial points
+    u.points = 0;
 
     fwrite(&u, sizeof(u), 1, fp);
 
@@ -31,14 +36,21 @@ void addUser() {
 
 // Reward system
 void rewardUser(int userId) {
-    FILE *fp = fopen("users.txt", "r");
-    FILE *temp = fopen("temp.txt", "w");
+    FILE *fp = fopen("users.txt", "rb");
+    FILE *temp = fopen("temp.txt", "wb");
+
+    if (fp == NULL || temp == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
 
     struct User u;
+    int found = 0;
 
     while (fread(&u, sizeof(u), 1, fp)) {
         if (u.id == userId) {
-            u.points += 10;  // reward points
+            u.points += 10;
+            found = 1;
             printf("User rewarded! New points: %d\n", u.points);
         }
         fwrite(&u, sizeof(u), 1, temp);
@@ -49,22 +61,8 @@ void rewardUser(int userId) {
 
     remove("users.txt");
     rename("temp.txt", "users.txt");
-}
 
-// Main function
-int main() {
-    int choice, id;
-
-    printf("1. Add User\n2. Reward User\n");
-    scanf("%d", &choice);
-
-    if (choice == 1) {
-        addUser();
-    } else if (choice == 2) {
-        printf("Enter User ID: ");
-        scanf("%d", &id);
-        rewardUser(id);
+    if (!found) {
+        printf("User not found!\n");
     }
-
-    return 0;
 }
